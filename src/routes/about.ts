910e7fcaa0a0656d6993c23a64dc068d8b45133d
ehsan <ehsan@raychat.io>
2021-07-18 01:32:01 +0430

    about router

---
+++ src/routes/about.ts
@@ -0,0 +1,28 @@
+// Packages
+import { Router } from 'express';
+
+// Controllers
+import AboutController from "../controllers/about";
+
+// Middlewares
+
+// Consts
+const router = Router();
+enum Names {
+  CREATE = '/create',
+  FIND_ONE = '/findOne/:id',
+  FIND = '/find',
+  UPDATE = '/update/:id',
+  REMOVE = '/remove/:id',
+}
+const About: any = new AboutController();
+
+router
+  .post(Names.CREATE, About.create)
+  .get(Names.FIND, About.find)
+  .get(Names.FIND_ONE, About.findOne)
+  .put(Names.UPDATE, About.update)
+  .delete(Names.REMOVE, About.remove);
+
+export default router;
+
