e87aae49b35acf7e9edd4ae6da6d5855bb86e59a
ehsan <ehsan@raychat.io>
2021-07-18 01:06:19 +0430

    news model

--- src/models/news.ts
+++ src/models/news.ts
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 import * as paginate from "mongoose-paginate";
 import * as timestamp from "mongoose-timestamp";
 
-const userSchema = new Schema(
+const newsSchema = new Schema(
   {
     email: { type: String, required: true },
     password: { type: String, required: true },
@@ -17,10 +17,10 @@ const userSchema = new Schema(
   { versionKey: false }
 );
 
-userSchema.plugin(paginate);
-userSchema.plugin(timestamp);
+newsSchema.plugin(paginate);
+newsSchema.plugin(timestamp);
 
-const UserModel = model("users", userSchema);
-UserModel.createIndexes();
+const NewsModel = model("news", newsSchema);
+NewsModel.createIndexes();
 
-export default UserModel;
+export default NewsModel;
