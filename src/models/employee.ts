8568faceb7717730e551386a40a22bb4620cecf0
ehsan <ehsan@raychat.io>
2021-07-18 01:06:02 +0430

    employee model

--- src/models/employee.ts
+++ src/models/employee.ts
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 import * as paginate from "mongoose-paginate";
 import * as timestamp from "mongoose-timestamp";
 
-const userSchema = new Schema(
+const employeeSchema = new Schema(
   {
     email: { type: String, required: true },
     password: { type: String, required: true },
@@ -17,10 +17,10 @@ const userSchema = new Schema(
   { versionKey: false }
 );
 
-userSchema.plugin(paginate);
-userSchema.plugin(timestamp);
+employeeSchema.plugin(paginate);
+employeeSchema.plugin(timestamp);
 
-const UserModel = model("users", userSchema);
-UserModel.createIndexes();
+const EmployeeModel = model("employee", employeeSchema);
+EmployeeModel.createIndexes();
 
-export default UserModel;
+export default EmployeeModel;
